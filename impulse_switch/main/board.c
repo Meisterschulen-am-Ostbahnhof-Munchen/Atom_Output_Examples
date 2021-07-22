4abfcf698bb168e95d9897317042efccb85a72a0
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-07-22 17:13:02 +0200

    Button Callback working !

--- impulse_switch/main/board.c
+++ impulse_switch/main/board.c
@@ -17,50 +17,18 @@
 
 #define TAG "BOARD"
 
-#define BUTTON_IO_NUM           0
-#define BUTTON_ACTIVE_LEVEL     0
+#define BUTTON_IO_NUM           32		// Pin 32.
+#define BUTTON_ACTIVE_LEVEL     0		//Masse-Geschalten
 
-extern void example_ble_mesh_send_gen_onoff_set(void);
 
-struct _led_state led_state[3] = {
-    { LED_OFF, LED_OFF, LED_R, "red"   },
-    { LED_OFF, LED_OFF, LED_G, "green" },
-    { LED_OFF, LED_OFF, LED_B, "blue"  },
-};
 
-void board_led_operation(uint8_t pin, uint8_t onoff)
-{
-    for (int i = 0; i < ARRAY_SIZE(led_state); i++) {
-        if (led_state[i].pin != pin) {
-            continue;
-        }
-        if (onoff == led_state[i].previous) {
-            ESP_LOGW(TAG, "led %s is already %s",
-                led_state[i].name, (onoff ? "on" : "off"));
-            return;
-        }
-        gpio_set_level(pin, onoff);
-        led_state[i].previous = onoff;
-        return;
-    }
-    ESP_LOGE(TAG, "LED is not found!");
-}
 
-static void board_led_init(void)
-{
-    for (int i = 0; i < ARRAY_SIZE(led_state); i++) {
-        gpio_reset_pin(led_state[i].pin);
-        gpio_set_direction(led_state[i].pin, GPIO_MODE_OUTPUT);
-        gpio_set_level(led_state[i].pin, LED_OFF);
-        led_state[i].previous = LED_OFF;
-    }
-}
+
 
 static void button_tap_cb(void* arg)
 {
     ESP_LOGI(TAG, "tap cb (%s)", (char *)arg);
 
-    example_ble_mesh_send_gen_onoff_set();
 }
 
 static void board_button_init(void)
@@ -73,6 +41,5 @@ static void board_button_init(void)
 
 void board_init(void)
 {
-    board_led_init();
     board_button_init();
 }
--- impulse_switch/main/main.c
+++ impulse_switch/main/main.c
@@ -1,14 +1,38 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2017 Intel Corporation
+ * Additional Copyright (c) 2018 Espressif Systems (Shanghai) PTE LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_system.h"
+#include "board.h"
 
+
+static const char *TAG = "impulse_switch";
 
 #define BLINK_GPIO GPIO_NUM_19
 
 void app_main(void)
 {
+
+
+
+    ESP_LOGI(TAG, "Initializing...");
+
+    board_init();
+	
+	
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
        muxed to GPIO on reset already, but some default to other
        functions and need to be switched to GPIO. Consult the
