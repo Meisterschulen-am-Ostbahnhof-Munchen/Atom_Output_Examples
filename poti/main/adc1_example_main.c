5032355a66dd497d100cdb50abff6c6fd70fa687
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-07-22 17:42:46 +0200

    ADC_ATTEN_DB_11  = 3,  /*!<The input voltage of ADC will be attenuated, extending the range of measurement to up to  approx. 2600 mV. */

--- poti/main/adc1_example_main.c
+++ poti/main/adc1_example_main.c
@@ -20,7 +20,7 @@
 static esp_adc_cal_characteristics_t *adc_chars;
 static const adc_channel_t channel = ADC_CHANNEL_4;     //GPIO32 if ADC1
 static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
-static const adc_atten_t atten = ADC_ATTEN_DB_0;
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
 
 
 static void check_efuse(void)
