075b66f32f7ba7d93e58430b751bba572ddb8118
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-07-22 16:48:47 +0200

    this is a Blink with a Variable. !

--- impulse_switch/main/main.c
+++ impulse_switch/main/main.c
@@ -1,16 +1,26 @@
 #include "freertos/FreeRTOS.h"
-
+#include "freertos/task.h"
 #include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_system.h"
 
 
+#define BLINK_GPIO GPIO_NUM_19
 
 void app_main(void)
 {
-
-    gpio_set_direction(GPIO_NUM_19, GPIO_MODE_OUTPUT);
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     int level = 0;
     while (true) {
-        gpio_set_level(GPIO_NUM_19, level);
+        gpio_set_level(BLINK_GPIO, level);
         level = !level;
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
