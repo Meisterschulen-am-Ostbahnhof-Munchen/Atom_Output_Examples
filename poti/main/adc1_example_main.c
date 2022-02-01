8176a1f8e238c6bdf44a4c43725ce3af35ab4c24
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-02-01 07:38:28 +0100

    500Hz

--- poti/main/adc1_example_main.c
+++ poti/main/adc1_example_main.c
@@ -91,7 +91,7 @@ void app_main(void)
      */
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
-        .freq_hz = 5000,                      // frequency of PWM signal
+        .freq_hz = 500,                      // frequency of PWM signal
         .speed_mode = LEDC_HS_MODE,           // timer mode
         .timer_num = LEDC_HS_TIMER,            // timer index
         .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
